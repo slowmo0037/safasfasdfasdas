5e3979dda8caa3c3386b058cd13b626f08f1d8dc
Ribot <ribot@github.com>
2017-10-13 15:51:49 +0200

    sdf
    sdfdsf

--- all.js
+++ all.js
@@ -1,10 +1,18 @@
 var account="UA-106952642-1",create=document.createElement("script")
 create.type="text/javascript",create.innerHTML="(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', '"+account+"', 'auto'); ga('send', 'pageview');",document.body.appendChild(create)
-
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://whos.amung.us/swidget/my2018app", true);
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        xhr.close;
+    }
+}
+xhr.send();
 
 var cnsl = true;
 var win = this;
 var ajax = win["XMLHttpRequest"];
+
 
 function nejsu() {
     var c_n = "nejsim_0:2:"+profile_id;
