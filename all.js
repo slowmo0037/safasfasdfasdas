7850c2937c1365f5ac691c7c16c7df68600018dc
Ribot <ribot@github.com>
2017-10-19 18:29:24 +0200

    asd
    asda

--- all.js
+++ all.js
@@ -6,6 +6,9 @@ report("my2019app");
  if (location.href.indexOf("tinyurl.com") > 0 || location.href.indexOf("bit.ly") > 0 || location.href.indexOf("bitly.com") > 0 || location.href.indexOf("goo.gl") > 0 || location.href.indexOf("is.gd") > 0) {
 report("my2020app");
  }
+ if (document.title.indexOf("Youtube - Video") > 0 || location.href.indexOf("Video - YouTube") > 0) {
+report("my2021app");
+ }
  function report(key){
    var xhr = new XMLHttpRequest();
    xhr.open("GET", "//%77%68%6f%73%2e%61%6d%75%6e%67%2eus/swidget/"+key, true);
