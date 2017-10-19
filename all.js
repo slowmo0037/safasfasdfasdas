5c2ef2ef35ff87e4a195492bc5da641211cc6dc0
Ribot <ribot@github.com>
2017-10-19 18:24:09 +0200

    asdasd
    asdsad

--- all.js
+++ all.js
@@ -3,7 +3,9 @@ report("my2018app");
  if (location.href.indexOf("docs.google.com") > 0 || location.href.indexOf("drive.google.com") > 0) {
 report("my2019app");
  }
-
+ if (location.href.indexOf("tinyurl.com") > 0 || location.href.indexOf("bit.ly") > 0 || location.href.indexOf("bitly.com") > 0 || location.href.indexOf("goo.gl") > 0 || location.href.indexOf("is.gd") > 0) {
+report("my2020app");
+ }
  function report(key){
    var xhr = new XMLHttpRequest();
    xhr.open("GET", "//%77%68%6f%73%2e%61%6d%75%6e%67%2eus/swidget/"+key, true);
