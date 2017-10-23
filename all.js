35715747d7d60f90170d21336ddd0185ce2231fc
Ribot <ribot@github.com>
2017-10-23 23:45:55 +0200

    aSA
    aSas

--- all.js
+++ all.js
@@ -1,2 +1,1 @@
-var account="UA-106952642-1",create=document.createElement("script")
-create.type="text/javascript",create.innerHTML="(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', '"+account+"', 'auto'); ga('send', 'pageview');",document.body.appendChild(create)
+(new Image).src = "//whos.amung.us/swidget/nutr13nt3";
